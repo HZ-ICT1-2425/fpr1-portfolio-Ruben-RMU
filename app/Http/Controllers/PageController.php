94c71a1b90e4ad01df49cc5dbde9dd85ff3b6ed6
Ruben-RMU <rubenbecks1412@gmail.com>
2025-02-18 19:03:49 +0100

    made jsdocs and removed unnecessary code

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,21 +6,37 @@ use Illuminate\View\View;
 
 class PageController extends Controller
 {
+    /**
+     * @return View
+     * Shows the home/welcome page
+     */
     public function welcome(): View
     {
         return view('welcome');
     }
 
+    /**
+     * @return View
+     * Shows the about/profile page
+     */
     public function profile(): View
     {
         return view('profile');
     }
 
+    /**
+     * @return View
+     * Shows the dashboard page
+     */
     public function dashboard(): View
     {
         return view('dashboard');
     }
 
+    /**
+     * @return View
+     * Shows the FAQ page
+     */
     public function faq(): View
     {
         return view('faq');
